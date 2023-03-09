22b0ec5a2a35c1bd0bb0d398c90bc7775eb4e3cc
dnjeh <ehdnj.001@gmail.com>
2023-03-09 11:19:37 +0900

    Today's SRH python classs' (i/o)

---
+++ SRHclass/20230309.py
@@ -0,0 +1,76 @@
+print("hello world!")
+
+print(3+4)
+
+value = 1234
+print(value)
+print(value + 100)
+
+print(3+4)
+4*5
+a=1
+b=2
+print(a+b)
+
+4*5
+
+a = '강아지'
+b = '고양이'
+print(a)
+print(b)
+
+print('*       *')
+print(' *     * ')
+print('  *   *  ')
+print('   * *   ')
+print('    *    ')
+
+a = '강아지'
+b = '고양이'
+print(a, end='')
+print(b)
+
+print(34, end='^^')
+print(78)
+
+a=12
+b=34
+print(a, b)
+
+a=12
+b=34
+print(a, b, sep=',')
+
+s='서울'
+d='대전'
+g='대구'
+b='부산'
+print(s, d, g, b, sep=" 찍고 ")
+
+print(12, 34, 56, sep='^^', end='-->')
+print(78)
+
+a=13
+b=21
+print(a, b)
+print(a+b)
+print(a, b, sep='')
+print(a, b, end='')
+
+age = input("몇 살이세요? ")
+print(age)
+
+name = input("이름을 입력하세요: ")
+print("안녕하세요 " + name + " 님!")
+
+onefur = input("첫 번째 과일 입력: ")
+twofur = input("두 번째 과일 입력: ")
+print(onefur, "와(과)", twofur, "은(는) 내가 좋아하는 과일이다.")
+
+y=input("몇 년도? ")
+m=input("몇 월? ")
+d=input("몇 일? ")
+print(y, m, d, sep=" . ")
+
+age=input("몇 살이세요? ")
+print(age + age)
